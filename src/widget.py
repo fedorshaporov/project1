aa49cb809ecbc4e21e0849d4c9586d500d0d5e6c
Fedor Shaporov <shaporovfed@gmail.com>
2025-10-13 21:56:57 +0300

    init function get_date

--- src/widget.py
+++ src/widget.py
@@ -9,3 +9,12 @@ def mask_account_card(card_info: str) -> str:
         return mask_account(card_number)
     else:
         return mask_card(card_number)
+
+    def get_date(date_str: str) -> str:
+        """Конвертирует дату из ISO-формата в формат ДД.ММ.ГГГГ.
+
+        :param date_str: Дата в формате "2024-03-11T02:26:18.671407".
+        :return: Дата в формате "ДД.ММ.ГГГГ".
+        """
+        date_object = datetime.fromisoformat(date_str)
+        return date_object.strftime("%d.%m.%Y")
