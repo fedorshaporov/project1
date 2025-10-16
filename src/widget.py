f9393544ec8d143b3285e9662feb77d6a66ff63a
Fedor Shaporov <shaporovfed@gmail.com>
2025-10-16 10:29:50 +0300

    init proect creature widget

--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,13 @@
-def mask_account_card(card_info: str) -> str:
-    """Маскирует номер карты или счета.
+def mask_account_card(account_info: str) -> str:
+    """
+    Маскирует номер карты или счета.
 
-    :param card_info: Строка, содержащая тип и номер карты или счета.
+    :param account_info: Строка с типом и номером карты/счета.
     :return: Строка с замаскированным номером.
     """
-    card_type, card_number = card_info.split(maxsplit=1)
-    if 'Счет' in card_type:
-        return mask_account(card_number)
+    account_type, account_number = account_info.split(maxsplit=1)
+
+    if "Счет" in account_type:
+        return f"{account_type} {mask_account(account_number)}"
     else:
-        return mask_card(card_number)
-
-    def get_date(date_str: str) -> str:
-        """Конвертирует дату из ISO-формата в формат ДД.ММ.ГГГГ.
-
-        :param date_str: Дата в формате "2024-03-11T02:26:18.671407".
-        :return: Дата в формате "ДД.ММ.ГГГГ".
-        """
-        date_object = datetime.fromisoformat(date_str)
-        return date_object.strftime("%d.%m.%Y")
+        return f"{account_type} {mask_card(account_number)}"
