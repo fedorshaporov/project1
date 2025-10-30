d70e2ebfd41cb919c08969072ca35b56b1ac0ac8
Fedor Shaporov <shaporovfed@gmail.com>
2025-10-30 16:19:04 +0300

    add comm

--- src/widget.py
+++ src/widget.py
@@ -1,41 +1,26 @@
-from project1.src.masks import mask_account, mask_card  # Импортируйте функции маскировки
-
+from src.masks import mask_card, mask_account  # Предположим, что эти функции уже реализованы.
 
 def mask_account_card(info: str) -> str:
     """
     Маскирует номер карты или счета.
 
-
-    :param info: Строка, содержащая тип и номер карты или счета.
-    :return: Строка с замаскированным номером.
+    :param info: Строка формата 'Тип 1234567890123456'
+    :return: Строка с замаскированным номером
     """
-    card_types = ['Visa', 'MasterCard', 'Maestro']
+    type_, number = info.split(maxsplit=1)
 
-    # Извлечение номера карты/счета из строки
-    for card_type in card_types:
-        if info.startswith(card_type):
-            # Маскируем номер карты
-            card_number = info.split(' ')[-1]  # Получаем только номер
-            masked_number = mask_card(card_number)
-            return info.replace(card_number, masked_number)  # Возвращаем с замаскированным номером
+    if 'Счет' in type_:
+        return f"{type_} {mask_account(number)}"
+    else:  # Ожидаем, что это карта
+        return f"{type_} {mask_card(number)}"
 
-    # Маскируем номер счета
-    if info.startswith('Счет'):
-        account_number = info.split(' ')[-1]  # Получаем только номер
-        masked_account = mask_account(account_number)
-        return info.replace(account_number, masked_account)  # Возвращаем с замаскированным номером
-
-    return info  # Если ничего не найдено, возвращаем исходную строку
-
-
-def get_date(date_string: str) -> str:
+def get_date(date_str: str) -> str:
     """
-    Преобразует строку даты в формат ДД.ММ.ГГГГ.
+    Преобразует строку даты в формат 'ДД.ММ.ГГГГ'.
 
-
-    :param date_string: Дата в формате ISO.
-    :return: Дата в формате ДД.ММ.ГГГГ.
+    :param date_str: Дата в формате 'YYYY-MM-DDTHH:MM:SS'
+    :return: Дата в формате 'ДД.ММ.ГГГГ'
     """
     from datetime import datetime
-    date = datetime.fromisoformat(date_string)
+    date = datetime.fromisoformat(date_str)
     return date.strftime("%d.%m.%Y")
