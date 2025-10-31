7a9afdd9176f5636d5352ad6d1120ca85cbe6e1c
Fedor Shaporov <shaporovfed@gmail.com>
2025-10-31 20:14:18 +0300

    исправлена функция маскировки счета и карты

--- src/masks.py
+++ src/masks.py
@@ -7,7 +7,7 @@ def mask_card(card_number: str) -> str:
     :return: Маскированный номер карты.
     """
     if len(card_number) >= 10:
-        return f"{card_number[:6]}{'*' * (len(card_number) - 10)}{card_number[-4:]}"
+        return f"{card_number[:4]} {card_number[4:6]+"**"} **** {card_number[-4:]}"
     return '*' * len(card_number)
 
 
@@ -19,7 +19,7 @@ def mask_account(account_number: str) -> str:
     :return: Маскированный номер счета.
     """
     if len(account_number) > 4:
-        return '*' * (len(account_number) - 4) + account_number[-4:]
+        return '**'  + account_number[-4:]
     return account_number
 
 
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -2,7 +2,7 @@ from src.widget import mask_account_card, get_date
 
 
 def test_mask_account_card():
-    assert mask_account_card('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 6361'
+    assert mask_account_card('Mastercard 7000792289606361') == 'Mastercard 7000 79** **** 6361'
     assert mask_account_card('Счет 73654108430135874305') == 'Счет **4305'
 
 
