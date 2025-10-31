1c2c2094df146aeea8aad74effc8f1d733514932
Fedor Shaporov <shaporovfed@gmail.com>
2025-10-31 20:42:59 +0300

    добавлен тест на mask_account,mask_card

--- src/masks.py
+++ src/masks.py
@@ -21,26 +21,3 @@ def mask_account(account_number: str) -> str:
     if len(account_number) > 4:
         return '**'  + account_number[-4:]
     return account_number
-
-
-def mask_account_card(info: str) -> str:
-    """
-    Маскирует номер карты или счета в строке.
-
-    :param info: Строка, содержащая тип (карта или счет) и номер.
-    :return: Строка с замаскированным номером.
-    """
-    card_types = ['Visa', 'MasterCard', 'Maestro']
-
-    for card_type in card_types:
-        if info.startswith(card_type):
-            card_number = info.split(" ")[-1]
-            masked_number = mask_card(card_number)
-            return info.replace(card_number, masked_number)
-
-    if info.startswith('Счет'):
-        account_number = info.split(" ")[-1]
-        masked_account = mask_account(account_number)
-        return info.replace(account_number, masked_account)
-
-    return info  # Если ничего не найдено, возвращаем исходную строку
