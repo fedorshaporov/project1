e2511e73d0f89b8bed5dad941387e3ee29a6c2cd
Fedor Shaporov <shaporovfed@gmail.com>
2025-11-20 14:50:03 +0300

    добавлен модуль data_reader.py и тест test_data_reader.py

---
+++ src/data_reader.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from typing import List, Dict
+
+
+def read_financial_operations_from_csv(file_path: str) -> List[Dict]:
+    """Считывает финансовые операции из CSV файла.
+
+    Args:
+        file_path (str): Путь к входному файлу CSV.
+
+    Returns:
+        List[Dict]: Список словарей с транзакциями.
+    """
+    df = pd.read_csv(file_path)
+    return df.to_dict(orient='records')
+
+
+def read_financial_operations_from_excel(file_path: str) -> List[Dict]:
+    """Считывает финансовые операции из Excel файла.
+
+    Args:
+        file_path (str): Путь к входному файлу Excel.
+
+    Returns:
+        List[Dict]: Список словарей с транзакциями.
+    """
+    df = pd.read_excel(file_path)
+    return df.to_dict(orient='records')
